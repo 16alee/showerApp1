8f9171415612fd38d9110e63add9c48bd54b2a54
16alee <16alee@castilleja.org>
2015-07-02 17:53:34 -0700

    implemented core data and starting graph

---
+++ showerTuness/GraphView.swift
@@ -0,0 +1,21 @@
+//
+//  GraphView.swift
+//  
+//
+//  Created by Ashley Lee on 6/30/15.
+//
+//
+
+import UIKit
+
+class GraphView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
